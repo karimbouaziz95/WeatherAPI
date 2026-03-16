533f178fa2cbf4c5e337edf380adbe7a312ceda3
Karim <karimbouazizpip@gmail.com>
2026-03-16 12:41:21 +0100

    Refactor Flask app structure: update home route, add API endpoint example.

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template
 
-app = Flask("Website")
+app = Flask(__name__)
+
 
 @app.route("/")
 def home():
-    return render_template("tuto.html")
+    return render_template("home.html")
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
 
-app.run(debug=True)
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 25
+    return {
+        "station": station,
+        "date": date,
+        "temperature": temperature
+    }
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
